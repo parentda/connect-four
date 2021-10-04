b1c70b41d521fa57e41a9345a86caca956fc1e77
parentda <danielparent21@gmail.com>
2021-10-04 18:05:56 -0400

    added player_input test suite

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -5,8 +5,8 @@ require_relative '../lib/board'
 require_relative '../lib/player'
 
 describe Game do
+  subject(:game) { described_class.new(2) }
   describe '#create_player' do
-    subject(:game) { described_class.new(2) }
     context 'when creating a new player' do
       before do
         allow(game).to receive(:name_prompt)
@@ -20,6 +20,50 @@ describe Game do
 
       it 'adds new player to players array' do
         expect { game.create_player(1) }.to change { game.players.length }.by(1)
+      end
+    end
+  end
+
+  describe '#player_input' do
+    let(:valid_input) { '3' }
+    let(:invalid_input) { 'A' }
+
+    context 'when player input is valid' do
+      before { allow(game).to receive(:gets).and_return(valid_input) }
+
+      it 'stops loop and does not display error message' do
+        expect(game).not_to receive(:invalid_selection_prompt)
+        game.player_input
+      end
+
+      it 'returns valid column number' do
+        column = game.player_input
+        expected_output = valid_input.to_i
+        expect(column).to eq(expected_output)
+      end
+    end
+
+    context 'when player inputs an incorrect value once, then a valid input' do
+      before do
+        allow(game).to receive(:gets).and_return(invalid_input, valid_input)
+      end
+      it 'completes loop and displays error message once' do
+        expect(game).to receive(:invalid_selection_prompt).once
+        game.player_input
+      end
+    end
+
+    context 'when player inputs two incorrect values, then a valid input' do
+      before do
+        allow(game).to receive(:gets).and_return(
+          invalid_input,
+          invalid_input,
+          valid_input
+        )
+      end
+      it 'completes loop and displays error message twice' do
+        expect(game).to receive(:invalid_selection_prompt).twice
+        game.player_input
       end
     end
   end
