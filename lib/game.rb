ed77da420b31be5b117383946091ff6e12e25be1
parentda <danielparent21@gmail.com>
2021-10-04 18:18:22 -0400

    updated game_setup

--- lib/game.rb
+++ lib/game.rb
@@ -3,24 +3,26 @@ require_relative 'displayable'
 class Game
   include Displayable
 
-  attr_reader :players
+  attr_reader :players, :current_player
 
   def initialize(num_players)
     @num_players = num_players
     @players = []
+    @current_player = nil
     @markers = ["\e[31m\u25CF\e[0m", "\e[33m\u25CF\e[0m"]
     @board = Board.new(6, 7, 4)
   end
 
   def play
     game_setup
-    player_turns
+    game_loop
     game_end
   end
 
   def game_setup
     game_start_prompt
     @num_players.times { |num| create_player(num) }
+    @current_player = @players.first
   end
 
   def create_player(number)
@@ -31,9 +33,13 @@ class Game
 
   def player_input
     while (column = gets.chomp.to_i)
-      break column if @board.valid_move?(column)
+      return column if @board.valid_move?(column)
 
       invalid_selection_prompt
     end
   end
+
+  def player_turn; end
+
+  def game_loop; end
 end
